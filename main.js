10e8e296b5486f6291db458f7da15e1445b1b416
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2022-09-15 00:31:02 +0800

    Added cube rotation

--- main.js
+++ main.js
@@ -13,24 +13,22 @@ console.log(camera);
 console.log(renderer);
 
 const points = [
-    new THREE.Vector2(0,0),
     new THREE.Vector2(1,1),
+    new THREE.Vector2(2,3),
 ]
-
-const material = new THREE.LineBasicMaterial({color: 0x00ff00});
-const geometryLine = new THREE.BufferGeometry().setFromPoints(points);
-const line = new THREE.Line(geometryLine, material);
-scene.add(line);
 
 const material1 = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const geometryCube = new THREE.BoxGeometry(1, 1, 1);
 const cube = new THREE.Mesh(geometryCube, material1);
+cube.rotation.x = 0.3;
 scene.add(cube);
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.render(scene, camera);
+
+    cube.rotation.y += 0.01;
 }
 
 animate();
