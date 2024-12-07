e2f84e86d91462e8a1437009fc3e3eca36b46c86
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2024-12-08 00:04:34 +0800

    'Changes'

--- main.js
+++ main.js
@@ -3,51 +3,82 @@ let scene;
 let camera;
 let renderer;
 let group;
-
+let sun, sun_geom, sun_mat;
+let earth, earth_geom, earth_mat;
 let textureCube;
-let loader;
+let stars;
+let controls;
 
 init();
 animate();
 
+document.addEventListener('mousedown', function(event) {
+    console.log(event.buttons)
+    if (event.buttons = 1) {
+        addEventListener("mousemove", moved);
+    }
+})
+
+function moved(event) {
+    if (event.buttons != 1) {
+        removeEventListener("mousemove", moved);
+    } else {
+        /*
+        group.rotation.x += (parseInt(event.movementY) * 0.002);
+        group.rotation.y += (parseInt(event.movementX) * 0.002);
+        camera.lookAt(sun.position);
+        */
+        theta = - ( ( event.clientX - onMouseDownPosition.x ) * 0.5 ) + onMouseDownTheta;
+        phi = ( ( event.clientY - onMouseDownPosition.y ) * 0.5 ) + onMouseDownPhi;
+
+        phi = Math.min( 180, Math.max( 0, phi ) );
+
+        camera.position.x = radious * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+        camera.position.y = radious * Math.sin( phi * Math.PI / 360 );
+        camera.position.z = radious * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+        camera.updateMatrix();
+    }
+}
+
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    camera = new THREE.PerspectiveCamera(45, parseInt(window.innerWidth) / parseInt(window.innerHeight), 1, 10000);
     renderer = new THREE.WebGLRenderer();
+    group = new THREE.Object3D();
+    stars = new THREE.Object3D();
 
     scene.background = new THREE.Color(0xdddddd);
     document.body.appendChild(renderer.domElement);
 
-    camera.position.z = 50;
+    camera.position.z = 6300;
 
-    console.log(scene);
-    console.log(camera);
-    console.log(renderer);
+    sun_geom = new THREE.SphereGeometry(430, 30, 30);
+    sun_mat = new THREE.MeshNormalMaterial();
+    sun = new THREE.Mesh(sun_geom, sun_mat);
+    group.add(sun);
 
-    //const materialCube = new THREE.MeshBasicMaterial({color: 0x7fffb7});
-    //const materialCube = new THREE.MeshNormalMaterial();
+    earth_geom = new THREE.SphereGeometry(50, 20, 20);
+    earth_mat = new THREE.MeshNormalMaterial();
+    earth = new THREE.Mesh(earth_geom, earth_mat);
+    earth.position.x = 1500;
+    group.add(earth);
+
     const geometryCube = new THREE.BoxGeometry(5, 5, 5);
-    group = new THREE.Object3D();
-
     textureCube = new THREE.TextureLoader().load('texture/crate1.jpg');
-
-    const materialCube = new THREE.MeshBasicMaterial({map: textureCube, overdraw: true});
-
-    for (var i=0; i<100; i++) {
-        const cube = new THREE.Mesh(geometryCube, materialCube);
-        cube.position.x=Math.random()*100-50;
-        cube.position.y=Math.random()*100-50;
-        cube.position.z=Math.random()*100-50;
-        group.add(cube);
+    const materialCube = new THREE.MeshBasicMaterial({map: textureCube});
+    
+    for (var i=0; i<1000; i++) {
+        const earth = new THREE.Mesh(earth_geom, earth_mat);
+        earth.position.x=Math.random()*3000-1500;
+        earth.position.z=Math.random()*1400-700;
+        group.add(earth);
     }
-
-    scene.add(group)
+    scene.add(group);
 }
 function animate() {
     requestAnimationFrame(animate);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
+    renderer.setSize(parseInt(window.innerWidth), parseInt(window.innerHeight));
+    camera.aspect = parseInt(window.innerWidth) / parseInt(window.innerHeight);
     camera.updateProjectionMatrix();
-    group.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
